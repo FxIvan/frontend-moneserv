4d30855f8d9cf2689a5b076abe75320a6def1db5
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2022-09-30 22:57:25 -0300

    agregando pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import { Index } from './component';
-import { HashRouter, Route, Routes } from 'react-router-dom'
-import { ChargingData } from './component/chg_date';
-import { Solicitar } from './component/solicitar/solicitar';
 import { Router } from './router/route';
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-          <Route path='/reserva'       element={<Index/>} />
-          <Route path='/charging/date' element={<ChargingData/>} />
-          <Route path='/form/solicitud' element={<Solicitar/>} />
-      </Routes>
-  </HashRouter>
+    <Router/>
   );
 }
 
