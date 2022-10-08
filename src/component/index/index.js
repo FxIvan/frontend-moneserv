8c3820501d954da30ce9a4be545af53f0bc41579
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2022-10-08 19:29:21 -0300

    agregando seccion3

--- src/component/index/index.js
+++ src/component/index/index.js
@@ -3,6 +3,7 @@ import { Header } from './header'
 import '../../css/index.css'
 import { Seccion1 } from '../seccion1/seccion1'
 import { Seccion2 } from '../seccion2/seccion2'
+import { Seccion3 } from '../seccion3/seccion3'
 
 export const Index = () =>{
     return(
@@ -12,6 +13,9 @@ export const Index = () =>{
             </div>
             <div>
                 <Seccion2/>
+            </div>
+            <div>
+                <Seccion3/>
             </div>
         </div>
     )
---
+++ src/component/seccion3/seccion3.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import lemoncash from './img/lemoncash.png'
+import mercadopago from './img/mercadopago.png'
+import uala from './img/uala.jpg'
+
+import '../../css/seccion3.css'
+
+export const Seccion3 = () =>{
+    
+    return(
+        <div>
+            <div className='container-seccion3'>
+                <div className='container-seccion3-titulo'>
+                    <h3>Medios de Pago Aceptado</h3>
+                </div>
+                <div className='container-logo-seccion3'>
+                    <div>
+                       <h4>MERCADO <br/> PAGO</h4>
+                    </div>
+                    <div>
+                        <h4>UALA</h4>
+                    </div>
+                    <div>
+                        <h4>LEMON <br/> CASH</h4> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
